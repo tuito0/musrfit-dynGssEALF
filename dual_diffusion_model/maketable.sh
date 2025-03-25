2a42f80c5088dcc80aab3b77260480dcffc568e5
tuito0 <tuito@post.j-parc.jp>
2025-03-25 15:17:23 +0900

    Add files via upload

---
+++ dual_diffusion_model/maketable.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cat dynGssEALF2_tbl_v0.1.0.bin0 dynGssEALF2_tbl_v0.1.0.bin1 dynGssEALF2_tbl_v0.1.0.bin2 > dynGssEALF2_tbl_v0.1.0.bin
+rm dynGssEALF2_tbl_v0.1.0.bin0 dynGssEALF2_tbl_v0.1.0.bin1 dynGssEALF2_tbl_v0.1.0.bin2
+
